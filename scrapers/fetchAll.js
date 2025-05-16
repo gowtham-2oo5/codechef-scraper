4b4d887e67add6f87210d1be3f86f36dfa8523da
Ala Gowtham Siva Kumar <gowtham.ala12345@gmail.com>
2025-05-16 10:35:03 +0530

    Fixed fetchAll.js to use 3 existing fetchers

--- scrapers/fetchAll.js
+++ scrapers/fetchAll.js
@@ -1,30 +1,20 @@
-const axios = require("axios");
-const cheerio = require("cheerio");
 const fetchRecent = require("./fetchRecent");
+const fetchProfile = require("./fetchProfile");
 const fetchRatings = require("./fetchRatings");
 
 module.exports = async (handle) => {
   try {
-    const { data } = await axios.get(
-      `https://www.codechef.com/users/${handle}`
-    );
-    const $ = cheerio.load(data);
+    const profileData = await fetchProfile(handle);
 
-    const profile = $(".user-details-container");
-    const ratingRanks = $(".rating-ranks");
-
-    const name = profile.find("h2").text().trim();
-    const profileImage = profile.find("img").attr("src");
-    const currentRating = parseInt($(".rating-number").first().text());
-    const highestRating = parseInt(
-      $(".rating-number").parent().find("small").text().replace(/[^\d]/g, "")
-    );
-    const stars = $(".rating").first().text().trim();
-    const countryName = $(".user-country-name").text().trim();
-    const countryFlag = $(".user-country-flag").attr("src");
-    const globalRank = parseInt(ratingRanks.find("td").eq(0).text());
-    const countryRank = parseInt(ratingRanks.find("td").eq(1).text());
-
+    const name = profileData.name;
+    const profileImage = profileData.profileImage;
+    const currentRating = profileData.currentRating;
+    const highestRating = profileData.highestRating;
+    const stars = profileData.stars;
+    const countryName = profileData.countryName;
+    const countryFlag = profileData.countryFlag;
+    const globalRank = profileData.globalRank;
+    const countryRank = profileData.countryRank;
     const ratingData = (await fetchRatings(handle)).ratingData;
     const recentActivity = (await fetchRecent(handle)).recentActivity;
 
